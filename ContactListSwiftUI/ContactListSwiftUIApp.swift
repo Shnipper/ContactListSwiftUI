341a1d2c18b947936966cb818c81e2e186519680
Voloshenko Evgeniu <95745884+Shnipper@users.noreply.github.com>
2022-05-19 20:37:46 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Евгений Волошенко on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
